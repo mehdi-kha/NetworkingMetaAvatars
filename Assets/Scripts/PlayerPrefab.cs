ae519f4b88c9e2cf4e6b2b526bf59d0410c63d28
Mehdi Khadir <mehdi.khadir@hotmail.fr>
2023-01-25 20:45:59 +0100

    main: Synchronize the avatars motion

--- Assets/Scripts/PlayerPrefab.cs
+++ Assets/Scripts/PlayerPrefab.cs
@@ -3,141 +3,88 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using Unity.Collections;
+using Unity.Collections.LowLevel.Unsafe;
 using Unity.Netcode;
 using UnityEngine;
 using static Oculus.Avatar2.OvrAvatarEntity;
 
 public class PlayerPrefab : NetworkBehaviour
 {
-    [SerializeField] private OvrAvatarEntity RemoteAvatarPrefab;
-    private OvrAvatarEntity _remoteAvatar;
-    private OvrAvatarEntity _localAvatar;
-    private static readonly float[] StreamLodSnapshotIntervalSeconds = new float[OvrAvatarEntity.StreamLODCount] { 1f / 72, 2f / 72, 3f / 72, 4f / 72 };
-    private readonly float[] _streamLodSnapshotElapsedTime = new float[OvrAvatarEntity.StreamLODCount];
-    private const int MAX_PACKETS_PER_FRAME = 3;
+    [SerializeField] private SampleAvatarEntity RemoteAvatarPrefab;
+    private SampleAvatarEntity _remoteAvatar;
+    private SampleAvatarEntity _localAvatar;
+    private bool _isRemoteAvatarLoaded;
 
-    public void Start()
+    // This value will only be filled for the local player
+    private ulong _userId;
+    WaitForSeconds waitTime = new WaitForSeconds(.08f);
+
+    public override void OnNetworkSpawn()
     {
+        base.OnNetworkDespawn();
         if (!IsLocalPlayer)
         {
+            // Will only run on the player prefabs that are not the current local player
             _remoteAvatar = Instantiate(RemoteAvatarPrefab, transform);
             return;
         }
 
+        // Else, if we are in the local player's prefab, do the following
         // Find the local avatar
-        _localAvatar = FindObjectsOfType<OvrAvatarEntity>().FirstOrDefault(el => el.IsLocal);
+        _localAvatar = FindObjectsOfType<SampleAvatarEntity>().FirstOrDefault(el => el.IsLocal);
+
+        // Set the oculus id
+        OvrPlatformInit.InitializeOvrPlatform();
+        Oculus.Platform.Users.GetLoggedInUser().OnComplete(message =>
+        {
+            if (!message.IsError)
+            {
+                _userId = message.Data.ID;
+            }
+            else
+            {
+                var e = message.GetError();
+                OvrAvatarLog.LogError($"Error loading CDN avatar: {e.Message}. Falling back to local avatar");
+            }
+        });
+
+        // Start streaming
+        StartCoroutine(StreamAvatarData());
     }
 
     [ServerRpc(RequireOwnership = false)]
-    public void SendAvatarServerRpc(byte[] data, uint dataByteCount)
+    public void SendAvatarServerRpc(byte[] data, ulong userId)
     {
         // Serialization of the packet data is implemented in the PacketData itself
-        SetRemoteAvatarClientRpc(data, dataByteCount);
+        SetRemoteAvatarClientRpc(data, userId);
     }
 
     [ClientRpc]
-    public void SetRemoteAvatarClientRpc(byte[] data, uint dataByteCount)
+    public void SetRemoteAvatarClientRpc(byte[] data, ulong userId)
     {
         if (IsLocalPlayer)
         {
             return;
         }
 
-        NativeArray<byte> nativeByteArray = new NativeArray<byte>(data, Allocator.Persistent);
-        var dataSlice = new NativeSlice<byte>(nativeByteArray, 0, (int)dataByteCount);
-        _remoteAvatar.ApplyStreamData(in dataSlice);
-    }
-
-    private void LateUpdate()
-    {
-        if (!IsLocalPlayer)
+        if (!_isRemoteAvatarLoaded)
         {
-            return;
+            _remoteAvatar.LoadRemoteUserCdnAvatar(userId);
+            _isRemoteAvatarLoaded = true;
         }
 
-        // Local avatar has fully updated this frame and can send data to the network
-        SendSnapshot();
+        _remoteAvatar.ApplyStreamData(data);
     }
 
-    private void SendSnapshot()
+    private IEnumerator StreamAvatarData()
     {
-        if (!_localAvatar.HasJoints) { return; }
-
-        for (int streamLod = (int)StreamLOD.High; streamLod <= (int)StreamLOD.Low; ++streamLod)
+        while(true)
         {
-            int packetsSentThisFrame = 0;
-            _streamLodSnapshotElapsedTime[streamLod] += Time.unscaledDeltaTime;
-            while (_streamLodSnapshotElapsedTime[streamLod] > StreamLodSnapshotIntervalSeconds[streamLod])
-            {
-                SendPacket((StreamLOD)streamLod);
-                _streamLodSnapshotElapsedTime[streamLod] -= StreamLodSnapshotIntervalSeconds[streamLod];
-                if (++packetsSentThisFrame >= MAX_PACKETS_PER_FRAME)
-                {
-                    _streamLodSnapshotElapsedTime[streamLod] = 0;
-                    break;
-                }
-            }
+            var data = _localAvatar.RecordStreamData(_localAvatar.activeStreamLod);
+            SendAvatarServerRpc(data, _userId);
+            yield return waitTime;
         }
     }
-
-    private void SendPacket(StreamLOD lod)
-    {
-        var packet = GetPacketForEntityAtLOD(lod);
-
-        packet.dataByteCount = _localAvatar.RecordStreamData_AutoBuffer(lod, ref packet.data);
-        Debug.Assert(packet.dataByteCount > 0);
-
-        if (packet.Release())
-        {
-            SendAvatarServerRpc(packet.data.ToArray(), packet.dataByteCount);
-        }
-    }
-
-    private PacketData GetPacketForEntityAtLOD(StreamLOD lod)
-    {
-        PacketData packet = new PacketData();
-        packet.lod = lod;
-        return packet.Retain();
-    }
-
-    public class PacketData : IDisposable
-    {
-        public NativeArray<byte> data;
-        public StreamLOD lod;
-        public UInt32 dataByteCount;
-
-        private uint refCount = 0;
-
-        public PacketData() { }
-
-        ~PacketData() {
-            Dispose(false);
-        }
-
-        public void Dispose() {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-
-        private void Dispose(bool disposing) {
-            if (data.IsCreated)
-            {
-                data.Dispose();
-            }
-            data = default;
-        }
-
-        public bool Unretained => refCount == 0;
-        public PacketData Retain() { ++refCount; return this; }
-        public bool Release() {
-            return --refCount == 0;
-        }
-    }
-    class LoopbackState
-    {
-        public List<PacketData> packetQueue = new List<PacketData>(64);
-        public StreamLOD requestedLod = StreamLOD.Low;
-        public float smoothedPlaybackDelay = 0f;
-    };
 }
